e833da81772c2a38e436579a329a089957df7aa2
Jessica Kiroff <jesskiroff@gmail.com>
2024-09-02 14:16:13 -0400

    render popular and veggies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-
+import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
      <h1>Recipe app</h1>
+     <Home/>
     </div>
   );
 }
---
+++ src/components/Veggie.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Veggie() {
+  return (
+    <div>
+      Veggie
+    </div>
+  )
+  
+}
+
+export default Veggie;
